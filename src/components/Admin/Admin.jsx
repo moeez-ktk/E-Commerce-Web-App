49c75e3c5c1345f2990b969c6b988bdd33ec2005
Moeezktk <moeezkhattak86@gmail.com>
2024-01-02 17:35:16 +0500

    adding tables

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -180,6 +180,8 @@ const stackedOptions = {
   borderSkipped: "round", // Use rounded corners
 };
 
+// Tabs Data
+
 const tabs = [
   { logo: <FaChartBar className="tab_logo" />, name: "Statistics" },
   { logo: <FaTruck className="tab_logo" />, name: "Orders" },
@@ -189,12 +191,86 @@ const tabs = [
 
 const Tab = ({ logo, name }) => {
   return (
-    <div className="tab">
+    <div className="tab ds">
       {logo}
       {name}
     </div>
   );
 };
+
+// Tables Data
+
+const products = [
+  {
+    id: 1,
+    name: "Product 1",
+    price: 20,
+    stock: 50,
+    sold: 10,
+    imageUrl:
+      "https://images.unsplash.com/photo-1434389677669-e08b4cac3105?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y2xvdGhlc3xlbnwwfHwwfHx8MA%3D%3D",
+  },
+  {
+    id: 2,
+    name: "Product 2",
+    price: 30,
+    stock: 40,
+    sold: 20,
+    imageUrl:
+      "https://images.unsplash.com/photo-1434389677669-e08b4cac3105?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y2xvdGhlc3xlbnwwfHwwfHx8MA%3D%3D",
+  },
+];
+
+const orders = [
+  {
+    orderId: 1,
+    items: [101, 102, 105],
+    customerName: "John Doe",
+    orderDate: "2024-01-01",
+    shippingAddress: "123 Main St, Cityville",
+    totalAmount: 150.99,
+    status: "Shipped",
+    paymentMethod: "Credit Card",
+  },
+  {
+    orderId: 2,
+    items: [103, 104],
+    customerName: "Jane Smith",
+    orderDate: "2024-01-02",
+    shippingAddress: "456 Oak St, Townsville",
+    totalAmount: 75.5,
+    status: "Processing",
+    paymentMethod: "PayPal",
+  },
+  // Add more orders as needed
+];
+
+const customers = [
+  {
+    customerId: 1,
+    name: "John Doe",
+    email: "john.doe@example.com",
+    phoneNumber: "123-456-7890",
+    address: "123 Main St, Cityville",
+    totalOrders: 10,
+    totalSpend: 500.99,
+    registrationDate: "2024-01-01",
+    lastLoginDate: "2024-01-15",
+    status: "Active",
+  },
+  {
+    customerId: 2,
+    name: "Jane Smith",
+    email: "jane.smith@example.com",
+    phoneNumber: "987-654-3210",
+    address: "456 Oak St, Townsville",
+    totalOrders: 5,
+    totalSpend: 250.5,
+    registrationDate: "2024-01-02",
+    lastLoginDate: "2024-01-14",
+    status: "Inactive",
+  },
+];
 
 const TopBar = () => {
   // SEARCH BAR
@@ -214,7 +290,8 @@ const TopBar = () => {
         <button className="ds" onClick={handleSearch}>
           <IoSearchSharp className="search_icon" />
         </button>
-        <input className="ds"
+        <input
+          className="ds"
           type="text"
           placeholder="Search"
           value={query}
@@ -236,7 +313,7 @@ const Admin = () => {
             LOGO
           </div>
 
-          <div id="tabs">
+          <div id="tabs" className="ds">
             {tabs.map((tab) => (
               <Tab logo={tab.logo} name={tab.name} />
             ))}
@@ -335,8 +412,7 @@ const Admin = () => {
           <div className="item6 subcontainer">
             <div className="item6_text">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
-              amet omnis eum quos saepe veniam. Cum, a. Esse cupiditate, eos
-              illo amet officia labore est eaque corporis laborum iusto iure!
+              is laborum iusto iure!
             </div>
             <div>
               <Doughnut
@@ -346,6 +422,116 @@ const Admin = () => {
               />
             </div>
           </div>
+          {/* <div className="container2 ds"> */}
+            <div className="item7 subcontainer2">
+              <table>
+                <thead>
+                  <tr>
+                    <th>Image</th>
+                    <th>Name</th>
+                    <th>Price</th>
+                    <th>Items in Stock</th>
+                    <th>Items Sold</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {products.map((product) => (
+                    <tr key={product.id}>
+                      <td>
+                        <img
+                          src={product.imageUrl}
+                          alt={product.name}
+                          style={{ width: "50px", height: "50px" }}
+                        />
+                      </td>
+                      <td>{product.name}</td>
+                      <td>${product.price}</td>
+                      <td>{product.stock}</td>
+                      <td>{product.sold}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+
+            <div className="item8 subcontainer2">
+              <table>
+                <thead>
+                  <tr>
+                    <th>Order ID</th>
+                    <th>Items</th>
+                    <th>Customer Name</th>
+                    <th className="hide_col">Order Date</th>
+                    <th>Shipping Address</th>
+                    <th>Total Amount</th>
+                    <th>Status</th>
+                    <th className="hide_col">Payment Method</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {orders.map((order) => (
+                    <tr key={order.orderId}>
+                      <td>{order.orderId}</td>
+                      <td>{order.items.join(", ")}</td>
+                      <td>{order.customerName}</td>
+                      <td className="hide_col">{order.orderDate}</td>
+                      <td>{order.shippingAddress}</td>
+                      <td>${order.totalAmount.toFixed(2)}</td>
+                      <td>{order.status}</td>
+                      <td className="hide_col">{order.paymentMethod}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+
+            <div className="item9 subcontainer2">
+              <table>
+                <thead>
+                  <tr>
+                    <th>Customer ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone Number</th>
+                    <th className="hide_col">Address</th>
+                    <th className="hide_col">Total Orders</th>
+                    <th className="hide_col">Total Spend</th>
+                    <th className="hide_col">Registration Date</th>
+                    <th className="hide_col">Last Login Date</th>
+                    <th className="hide_col">Status</th>
+                    <th>Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {customers.map((customer) => (
+                    <tr key={customer.customerId}>
+                      <td>{customer.customerId}</td>
+                      <td>{customer.name}</td>
+                      <td>{customer.email}</td>
+                      <td>{customer.phoneNumber}</td>
+                      <td className="hide_col">{customer.address}</td>
+                      <td className="hide_col">{customer.totalOrders}</td>
+                      <td className="hide_col">
+                        ${customer.totalSpend.toFixed(2)}
+                      </td>
+                      <td className="hide_col">{customer.registrationDate}</td>
+                      <td className="hide_col">{customer.lastLoginDate}</td>
+                      <td className="hide_col">{customer.status}</td>
+                      <td>
+                        {/* Add action buttons/links here */}
+                        <button
+                          onClick={() => handleViewDetails(customer.customerId)}
+                        >
+                          View Details
+                        </button>
+                        {/* Add other action buttons/links as needed */}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          {/* </div> */}
         </div>
       </div>
     </>
