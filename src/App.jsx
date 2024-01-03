f7a9c1a1314a8b90ebb347388b9245cc8c7b6045
Moeezktk <moeezkhattak86@gmail.com>
2024-01-03 19:43:46 +0500

    created Cart Page, Created Cart Item Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Admin from './components/Admin/Admin';
+import Cart from './components/Cart/Cart';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route exact path='/' element={<Admin />} />
+        <Route exact path='/cart' element={<Cart />} />
       </Routes>
     </div>
   );
