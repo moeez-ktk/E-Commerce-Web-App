af1310120d96bf3ab72b6632c6f990710924a50a
rafiaashraf <rafiaashraf122@gmail.com>
2024-01-05 17:47:23 +0500

    Customer Login Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Admin from './components/Admin/Admin';
+import Login from './components/Login/Login';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route exact path='/' element={<Admin />} />
+        <Route exact path='/Login' element={<Login />} />
       </Routes>
     </div>
   );
